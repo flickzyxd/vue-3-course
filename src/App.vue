3e6fde0599779e0759ec98dd8294d2b2cf2f2adf
flickzyxd <stepanfominykh2000@gmail.com>
2025-05-17 12:16:36 +0500

    2243 day 1

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,39 @@
 <template>
-
+  <div>
+    <div class="post" v-for="post in posts">
+      <div><strong>Название:</strong>{{ post.title }}</div>
+      <div><strong>Описание:</strong>{{ post.body }}</div>
+    </div>
+  </div>
 </template>
 
-<script setup>
+<script>
+export default {
+  data() {
+    return {
+      posts: [
+        {id: 1, title: 'VueJs', body: 'Описание 1'},
+        {id: 1, title: 'VueJs', body: 'Описание 2'},
+        {id: 1, title: 'VueJs', body: 'Описание 3'},
+      ]
+    }
+  },
+  methods: {
+
+  }
+}
 </script>
 
-<style scoped>
+<style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+}
 </style>
