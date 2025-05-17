2fd44aa2fd4550d89a7f0f7aff8f0de27c2de070
flickzyxd <stepanfominykh2000@gmail.com>
2025-05-17 15:10:19 +0500

    3140 day 1

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
+  <div class="app">
+    <form @submit.prevent>
+      <h4>Создание поста</h4>
+      <input v-bind:value="title" @input="title = $event.target.value" class="input" type="text" placeholder="Название">
+      <input v-bind:value="body" @input="body = $event.target.value" class="input" type="text" placeholder="Описание">
+      <button class="btn" @click="createPost">Создать</button>
+    </form>
     <div class="post" v-for="post in posts">
       <div><strong>Название:</strong>{{ post.title }}</div>
       <div><strong>Описание:</strong>{{ post.body }}</div>
@@ -15,11 +21,26 @@ export default {
         {id: 1, title: 'VueJs', body: 'Описание 1'},
         {id: 1, title: 'VueJs', body: 'Описание 2'},
         {id: 1, title: 'VueJs', body: 'Описание 3'},
-      ]
+        {id: 1, title: 'VueJs', body: 'Описание 4'},
+      ],
+      title: '',
+      body: '',
     }
   },
   methods: {
-
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body,
+      }
+      this.posts.push(newPost);
+      this.title = '';
+      this.body = '';
+    },
+    inputTitle(event) {
+      this.title = event.target.value;
+    }
   }
 }
 </script>
@@ -31,9 +52,34 @@ export default {
   box-sizing: border-box;
 }
 
+.app {
+  padding: 20px;
+}
+
 .post {
   padding: 15px;
   border: 2px solid teal;
   margin-top: 15px;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.input {
+  width: 100%;
+  border: 2px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+
+.btn {
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 2px solid teal
 }
 </style>
