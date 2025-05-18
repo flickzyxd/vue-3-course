bec6d5a82a33dc10aad46202fca5e32eb4a6d6c2
flickzyxd <stepanfominykh2000@gmail.com>
2025-05-18 09:54:54 +0500

    5357 day 2 / add UI library, add MyButton component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="app">
-    <post-form></post-form>
-    <post-list :posts="posts"></post-list>
+    <post-form
+        @create="createPost"
+    />
+    <post-list
+        :posts="posts"
+    />
   </div>
 </template>
 
@@ -21,24 +25,12 @@ export default {
         {id: 1, title: 'VueJs', body: 'Описание 3'},
         {id: 1, title: 'VueJs', body: 'Описание 4'},
       ],
-      title: '',
-      body: '',
     }
   },
   methods: {
-    createPost() {
-      const newPost = {
-        id: Date.now(),
-        title: this.title,
-        body: this.body,
-      }
-      this.posts.push(newPost);
-      this.title = '';
-      this.body = '';
+    createPost(post) {
+      this.posts.push(post);
     },
-    inputTitle(event) {
-      this.title = event.target.value;
-    }
   }
 }
 </script>
--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -1,17 +1,46 @@
 <template>
   <form @submit.prevent>
     <h4>Создание поста</h4>
-    <input :value="post.title" @input="post.title = $event.target.value" class="input" type="text" placeholder="Название">
-    <input :value="post.body" @input="post.body = $event.target.value" class="input" type="text" placeholder="Описание">
-    <button class="btn" >Создать</button>
+    <input
+        v-model="post.title"
+        class="input"
+        type="text"
+        placeholder="Название">
+    <input
+        v-model="post.body"
+        class="input"
+        type="text"
+        placeholder="Описание">
+    <my-button
+        class="btn"
+        style="align-self: flex-end; margin-top: 15px;"
+        @click="createPost"
+    >
+      Создать
+    </my-button>
   </form>
 </template>
 
 <script>
+import MyButton from "@/components/UI/MyButton.vue";
+
 export default {
+  components: {
+    MyButton
+  },
   data() {
     return {
       post: {
+        title: '',
+        body: '',
+      }
+    }
+  },
+  methods: {
+    createPost() {
+      this.post.id = Date.now();
+      this.$emit('create', this.post,)
+      this.post = {
         title: '',
         body: '',
       }
@@ -26,14 +55,5 @@ export default {
   border: 2px solid teal;
   padding: 10px 15px;
   margin-top: 15px;
-}
-
-.btn {
-  margin-top: 15px;
-  align-self: flex-end;
-  padding: 10px 15px;
-  background: none;
-  color: teal;
-  border: 2px solid teal
 }
 </style>
